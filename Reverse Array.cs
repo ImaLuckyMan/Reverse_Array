5c84be8d6f9c94775fb78e5532b96bda397d6842
ImaLuckyMan <Ima_Lucky_Man@yahoo.com>
2020-02-19 17:17:17 -0800

    Add project files.

---
+++ Reverse Array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reverse_Array
+{
+    class Reverse_Array
+    {
+        static void Main(string[] args)             // Enter point for the program
+        {
+            int[] given = { 1, 2, 3, 4 };           // Test numbers as given input
+            Reversed(given);                        // Used to input array into method below
+        }
+
+        public static void Reversed(int[] input)    // Method to reverse the order of input
+        {
+            string output="";                       // Creates an empty place holder for the reversed input string
+            int i;                                  // Standard int for loop Indices reference
+
+            for (i=input.Length-1;i>=0;--i)         // for loop steps backward through input begining at end and working backwards
+            {
+                output += input[i];                 // adds the int at each index to the output string one at a time
+            }
+            Console.WriteLine(output);              // outputs the result to the screen
+
+        }
+
+
+    }
+}
